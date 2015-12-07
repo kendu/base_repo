612c25b14524ffc4b758d18c56ae3e16e8d77640
Matevž Golavšek <matevz.golavsek@kendu.si>
2015-12-07 09:54:47 +0100

    Added gulpif to tasks

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,7 @@ var gulp            = require('gulp'),
     autoprefixer    = require('gulp-autoprefixer');
     imagemin        = require('gulp-imagemin');
     pngquant        = require('imagemin-pngquant');
-
-
-var debug = (process.env.ENVIRONMENT == 'local');
-
-process.stdout.write(
-    "Running Gulp with debug="+debug+", env=" + process.env.ENVIRONMENT + "\n"
-);
-
+    build           = process.argv[2] === 'build';
 
 var cssLinks = [
     'bower_components/fontawesome/css/font-awesome.min.css',
@@ -42,57 +35,38 @@ var fontLinks = [
 /* Front tasks */
 gulp.task('styles', function() {
   return gulp.src(cssLinks)
-    .pipe(gulpif(debug, sourcemaps.init({loadMaps: true, debug: true})))
+    .pipe(sourcemaps.init({loadMaps: true, debug: true}))
     .pipe(concat('front' + '.temp.scss'))
     .pipe(sass({
         errLogToConsole: true
     }))
     .pipe(concat('main.css'))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(gulpif(debug, sourcemaps.write('./')))
+    .pipe(gulpif(build, minifyCSS()))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('build/'))
     .pipe(livereload(
         port = '35729'
     ));
-});
-
-gulp.task('styles-build', function() {
-  return gulp.src(cssLinks)
-    .pipe(sass({
-        errLogToConsole: true
-    }))
-    .pipe(concat('main.css'))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('build/'));
 });
 
 gulp.task('scripts', function() {
   return gulp.src(jsLinks)
-    .pipe(gulpif(debug, sourcemaps.init({loadMaps: true, debug: true})))
+    .pipe(sourcemaps.init({loadMaps: true, debug: true}))
     .pipe(concat('front' + '.temp.js'))
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
-    .pipe(gulpif(debug, sourcemaps.write('./')))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulpif(build, uglify().on('error', function(err) {
+        console.log(err.message);
+        this.emit('end');
+    })))
     .pipe(gulp.dest('build/'))
     .pipe(livereload(
         port = '35729'
     ));
 });
-
-gulp.task('scripts-build', function() {
-  return gulp.src(jsLinks)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(uglify().on('error', function(err) {
-        console.log(err.message);
-        this.emit('end');
-    }))
-    .pipe(gulp.dest('build/'))
-});
-
 
 /* Fonts */
 gulp.task('fonts', function() {
@@ -104,17 +78,11 @@ gulp.task('fonts', function() {
 /* Images */
 gulp.task('images', function() {
     return gulp.src('web/images/**/*.{gif,jpg,png,svg}')
-    .pipe(gulp.dest('build/images'));
-})
-
-
-gulp.task('images-build', function() {
-    return gulp.src('web/images/**/*.{gif,jpg,png,svg}')
-    .pipe(imagemin({
-		progressive: true,
-		svgoPlugins: [{removeViewBox: false}],
-		use: [pngquant()]
-	}))
+    .pipe(gulpif(build, imagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
+    })))
     .pipe(gulp.dest('build/images'));
 })
 
@@ -131,4 +99,4 @@ gulp.task('default', ['fonts', 'images', 'styles', 'scripts'], function() {
     });
 });
 
-gulp.task('build', ['fonts', 'images-build', 'styles-build', 'scripts-build' ]);
+gulp.task('build', ['fonts', 'images', 'styles', 'scripts' ]);
